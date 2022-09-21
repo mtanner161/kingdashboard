c2db4af4d5ca4e6fd46d8512d4e94442e4228126
mtanner161 <m.tanner161@gmail.com>
2022-09-21 09:46:19 -0500

    adding daily production powerbi embed

--- src/componants/pages/Operations.js
+++ src/componants/pages/Operations.js
@@ -4,6 +4,15 @@ const Operations = () => {
   return (
     <div>
       <h1>Operations</h1>
+      <h2>Production Report</h2>
+      <iframe
+        title="Production Final Update"
+        width="1024"
+        height="775"
+        src="https://app.powerbi.com/view?r=eyJrIjoiNjUwNTk4MjctMzdhMS00MWUyLWE5MGMtMDBmMmEyNjcxMjNhIiwidCI6IjEwMDMzYzcyLWYzNDgtNGY2Ny04OWZlLTM4NzFlZmJhODRjMiJ9&pageName=ReportSection511201b4448957ebc4f1"
+        frameborder="0"
+        allowFullScreen="true"
+      ></iframe>
     </div>
   );
 };
