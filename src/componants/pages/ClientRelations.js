63fb347c76299fa1a4463d8196c436840ca3dda0
mtanner161 <m.tanner161@gmail.com>
2022-09-22 11:24:19 -0500

    creating button success render onClick event

--- src/componants/pages/ClientRelations.js
+++ src/componants/pages/ClientRelations.js
@@ -1,10 +1,22 @@
 import React from "react";
 import Button from "@mui/material/Button";
+import { useState } from "react";
 
 const ClientRelations = () => {
   const address = "Click this button to update all addresses in W Energy";
   const disturbutions =
     "Click this button to import monthly disturbutions in Junpier Square";
+
+  const [isShownAddress, setIsShownAddress] = useState(false);
+  const [isShownDis, setIsShownDis] = useState(false);
+
+  const handleClickAddress = (event) => {
+    setIsShownAddress((current) => !current);
+  };
+
+  const handleClickDisturbution = (event) => {
+    setIsShownDis((current) => !current);
+  };
 
   return (
     <div>
@@ -12,16 +24,32 @@ const ClientRelations = () => {
       <br></br>
       <h2>{address}</h2>
       <div>
-        <Button variant="contained" color="success">
+        <Button
+          variant="contained"
+          color="success"
+          onClick={handleClickAddress}
+        >
           Update Address
         </Button>
-        <br></br>
-        <br></br>
+        {isShownAddress && (
+          <div>
+            <h2>Sucessfully Run</h2>
+          </div>
+        )}
         <h2>{disturbutions}</h2>
         <div>
-          <Button variant="contained" color="success">
+          <Button
+            variant="contained"
+            color="success"
+            onClick={handleClickDisturbution}
+          >
             Get Monthly Disturbutions
           </Button>
+          {isShownDis && (
+            <div>
+              <h2>Sucessfully Run</h2>
+            </div>
+          )}
         </div>
       </div>
     </div>
