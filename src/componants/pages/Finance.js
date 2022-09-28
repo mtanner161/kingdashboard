ab9ab853b8513f3f16485b564810e00ce2269808
mtanner161 <m.tanner161@gmail.com>
2022-09-28 10:26:44 -0500

    adding wolfepak widget

--- src/componants/pages/Finance.js
+++ src/componants/pages/Finance.js
@@ -4,6 +4,12 @@ const Finance = () => {
   return (
     <div>
       <h1>Finance</h1>
+      <iframe
+        width="900px"
+        height="750px"
+        frameborder="0"
+        src="https://wpi.wolfepakcloud.com/app/main#/dashboards/627e6e5bde810a0037ccfbdc?embed=true"
+      ></iframe>
     </div>
   );
 };
