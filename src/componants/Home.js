f43814411d96dd88a44691781fafb4f45bb4829f
mtanner161 <m.tanner161@gmail.com>
2022-11-12 09:44:04 -0600

    working with API script for energyDashboard

--- src/componants/Home.js
+++ src/componants/Home.js
@@ -4,6 +4,21 @@ import Accordion from "./Accordion";
 import IconLogo from "./images/kocLogoRaw.png";
 
 const Home = () => {
+  const fetch = (...args) =>
+    import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+  // api v2
+  const url =
+    "https://api.eia.gov/v2/petroleum/stoc/wstk/data?api_key=f8127de985a95b35a603961cfd50cdbd&data[]=value&facets[duoarea][]=NUS&facets[series][]=WCESTUS1";
+
+  async function getEiaData(urlgood) {
+    const response = await fetch(urlgood);
+    return response.json();
+  }
+
+  const data = getEiaData(url);
+  console.log(data);
+
   return (
     <div className="background-color">
       <br></br>
